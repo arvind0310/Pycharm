d82c153f77510088c88d4d9d4010ee186bb314d3
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-09-21 22:36:06 +0530

    Create 03.py

---
+++ Practice/03.py
@@ -0,0 +1,125 @@
+# if __name__ == '__main__':
+#     l2 = []
+#     l1 = []
+
+
+# for i in range(int(input())):
+#     name = input()
+#     l1.append(name)
+#     score = float(input())
+#     l1.append(score)
+#     l2.append(l1)
+#
+# print(l2)
+# l1=[]
+# l2=[]
+# for i in range(2):
+#     name=input(f"enter the name {i+1} :")
+#     l1.append(name)
+#     score=float(input(f"enter the score {i+1}: "))
+#     l1.append(score)
+#     print(l1)
+    # l2.append(l1)
+    # l2.append(score)
+# print(l1)
+# l2.append(l1)
+# print(l2)
+# # print(max(l2))
+
+
+# l1=[]
+# l2=[]
+# for i in range(2):
+#     name=input(f"enter the name {i+1} :")
+#     l1.append(name)
+#     score=float(input(f"enter the score {i+1}: "))
+#     l1.append(score)
+#     # print(l1)
+#     l2.append(l1)
+#     l1=[]
+# print(l2)
+
+# if __name__ == '__main__':
+#
+#     n = []
+#     s = []
+#     for _ in range(int(input("n:"))):
+#         name = input("name")
+#         n.append(name)
+#         score = float(input("score"))
+#         s.append(score)
+#     a=max(s)
+#     b=min(s)
+#     for i in range(len(n)):
+#         # print(len(n))
+#         if s[i]<a and s[i]>b:
+#             a=s[i]
+#
+#     for j in range(len(n)):
+#         if s[j]==a:
+#             print(n[j])
+
+
+# if __name__ == '__main__':
+#     for _ in range(int(input())):
+#         name = input()
+#         score = float(input())
+
+# lst=[]
+# lst.insert(0,5)
+# lst.insert(1,10)
+# # print(lst)
+# lst.insert(0,6)
+# print(lst)
+# lst.remove(6)
+# lst.append(9)
+# lst.append(1)
+# lst.sort()
+# print(lst)
+# lst.pop()
+# lst.reverse()
+# print(lst)
+# # lst[0]=0
+# # print(lst)
+# if __name__ == '__main__':
+#     N = int(input())
+
+
+
+# x={}
+# x[1]="one"
+# x[3]="three"
+# print(x)
+
+# favlang = {}
+# a=(input("enter your fav language Arvind:"))
+# b=input("enter your fav language chandu:")
+# # c=int("enter your fav language sakshi")
+# # d=int("enter your fav language adamya")
+# favlang["arvind"]=a
+# favlang["chandu"]=b
+# print(favlang)
+
+# keys=["a","b",'c']
+# values=[1,2,3]
+# x=dict(zip(keys,values))
+# print(x)
+
+# list=[]
+# x=int(input("no of input:"))
+# for i in range(x):
+#     n=int(input())
+#     list.append(n)
+# print(max(list))
+
+# list=["Arvind","Ayush","queen"]
+# x=input("enter the user name:")
+# if x in list:
+#     print("user found!")
+# else:
+#     print("not found")
+# i=1
+# while(i<10):
+#     print("this is "+str(i))
+#     i=i+1
+
