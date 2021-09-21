d8860dd14f3b9c87096b422b6366bf4982d151c0
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-09-21 22:21:22 +0530

    Create OOPs_1.py

---
+++ OOPs_1.py
@@ -0,0 +1,58 @@
+# print(type(2))
+
+# class ec:
+#     def data(self):
+#         print(f" name is: {self.name} age is {self.age} roll no is {self.rollno}")
+#
+# arvind=ec()  #object instantiation
+# arvind.name="arvind"
+# arvind.age="22"
+# arvind.rollno="19"
+# arvind.data()   #fn(method) call '''{object.method()} '''
+# ec.data(arvind)
+#
+# ashu=ec()     #object instantiation
+# ashu.name="ashu"
+# ashu.age="21"
+# ashu.rollno="20"
+# ashu.data()   #fn(method) call
+# # ec.data(ashu)
+# # ec.data(self=ashu)
+
+# class ec:
+#     def data(self):
+#         print(f"name is {self.name} salary is {self.salary} ")
+#
+#
+#
+# arvind=ec()
+# arvind.name="arvind"
+# arvind.salary=100
+# arvind.data()
+
+# class ec:
+#     def __init__(self,name,salary):
+#         self.name=name
+#         self.salary=salary
+#     def data(self):
+#         print(f"name is {self.name} salary is {self.salary} ")
+#
+#
+# arvind=ec("arvind",100)
+# arvind.data()
+
+# class ec:
+#     def data(self,name, salary):
+#         print(f"name is {name} salary is {salary} ")
+#
+#
+# arvind=ec()
+# arvind.data("arvind",100)
+
+# class person:
+#     count=0
+#     def __init__(self):
+#         person.count+=1   #class_name.attribute_name
+# p1=person()
+# p2=person()
+# print(person.count)
