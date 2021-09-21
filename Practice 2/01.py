ab1af23f677c4f65c0a51ba83782da33458ddc95
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-09-21 22:51:29 +0530

    Create 01.py

---
+++ Practice 2/01.py
@@ -0,0 +1,74 @@
+# def getSum(n):
+#     sum = 0
+#     for i in n:
+#         # print(i)
+#         sum = sum + int(i)
+#     return sum
+#
+# n =input("enter a:")
+# print(getSum(n))
+
+
+# n =input("enter a:")
+# n="123"
+# sum=0
+# for i in n:
+#     # print(i)
+#     sum = sum + int(i)
+# print(sum)
+
+
+
+# for i in range(3):
+#     a=int(input("enter the first numb:"))
+#     b=int(input("enter the second numb:"))
+#     c=a^b
+#     print(f"XOR in decimal is {c}")
+#
+#     x=(bin(c)[2:])
+#     print(f"binary of {c} is {x}")
+#     # n =input("enter a:")
+#     # n="123"
+#     sum=0
+#     for i in x:
+#         # print(i)
+#         sum = sum + int(i)
+#     print("sum of digits are",sum)
+
+#_______________________+________________________________________________________________________
+
+
+
+
+
+# for i in range(3):
+#     a=int(input())
+#     b=int(input())
+#     c=a^b
+#     x=(bin(c)[2:])
+#     sum=0
+#     for i in x:
+#         sum = sum + int(i)
+#     print(sum)
+
+
+# a=1,2,3,4,1,1
+# print(a)
+# # print(a.count(1))
+
+# lst=[]
+# x=int(input())
+# for i in range(x):
+#     num=int(input())
+#     lst.append(num)
+# print(max(lst)
+# x="arvind"
+# for i in x:
+#     print(i,end="")
+
+# def addNumber(a,b):
+#     return int(a+b)//1
+#
+# a=float(input())
+# b=float(input())
+# print(addNumber(a,b))
