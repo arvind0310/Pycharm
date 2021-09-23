ec9483560a6b896872aa73455fd614b1f5b0fe1a
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-09-23 21:37:43 +0530

    Update 1.py

--- practice 3/1.py
+++ practice 3/1.py
@@ -1,3 +1,12 @@
+
+l=[1,1,2,1,3,3,3]
+for i in range(len(l)): #after 3 time ..no 3 will be there in list
+    l.remove(3)
+print(l)
+
+#errror
+
+
 l=[1,1,2,1,3,3,3]
 for i in range(l.count(3)):
     l.remove(3)
