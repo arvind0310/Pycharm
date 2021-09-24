c59cb40ba0e04aa5da2dbf91fcd6a3987c785675
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-09-24 13:12:43 +0530

    Update 1.py

--- practice 3/1.py
+++ practice 3/1.py
@@ -6,8 +6,30 @@ print(l)
 
 #errror
 
+#____________________________________+______________________________________________________________________________+___________________________________
+
 
 l=[1,1,2,1,3,3,3]
 for i in range(l.count(3)):
     l.remove(3)
 print(l)
+
+#____________________________________+______________________________________________________________________________+___________________________________
+
+n1=int(input("n1:"))
+n2=int(input("n2:"))
+l1=[]
+l2=[]
+
+for i in range(n1):
+    x=int(input("element: "))
+    l1.append(x)
+s1=set(l1)
+
+for i in range(n2):
+    y=int(input("element: "))
+    l2.append(y)
+s2=set(l2)
+
+s3=s1.intersection(s2) #will give common number among both given list
+print(s3)
